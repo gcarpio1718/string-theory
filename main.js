f8a108e875e5ef7b6fd2320acc449665e6627e0f
gcarpio1718 <gcarpio1221@gmail.com>
2022-02-17 10:58:04 -0500

    change indexed function

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const prompt = require('prompt-sync')();
 
-console.log(xify("hi there"));
+//xify function//
 
-function xify(str){
-    let strX = "";
-    str.length 
-    for(let count = 0; count < str.length; count++){
-        strX += 'x';
+// console.log(xify("hi there"));
+
+// function xify(str){
+//     let strX = "";
+//     str.length 
+//     for(let count = 0; count < str.length; count++){
+//         strX += 'x';
+//     }
+
+//     return strX
+//  }
+
+//indexed numbers function//
+function stringy (str){
+    let strX = ""
+
+    for (let i = 0; i < str.length; i++){
+        strX += i + str[i]
+
     }
+    console.log(strX)
+}
 
-    return strX
- }
+let phrase1 = 'hello';
+stringy (phrase1);
